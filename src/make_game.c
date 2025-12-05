338d91190665f4dd99bbe53e0c85b8a3ab31e01f
kosato <suprinter1229@gmail.com>
2025-12-06 05:10:53 +0900

    feat: 新機能の追加 ※次はアガりになるパターンの機能を作成していく

--- src/make_game.c
+++ src/make_game.c
@@ -1,43 +1,79 @@
 #include "make_game.h"
 
-// int decide_turn(void) {
-
-//     srand((unsigned int)time(NULL));
-
-//     int random_value = rand() % 2;
-
-//     if(random_value == 0) {
-
-//         printf("The draw is done. You're the first player!");
-
-//     } else {
-
-//         printf("The draw is done. You're the second player!");
-//     }
-//     return random_value;
-// }
-
-// player_turn() {
-
-
-// }
-
-// opponent_turn() {
-// }
-
-void outputBord(char board[ROWS][COLS]) {
+void outputBoard(char board[ROWS][COLS]) {
     int i;
     for (i = 0; i < ROWS; i++) {
         int j;
         for (j = 0; j < COLS; j++) {
-            printf("%c ", board[i][j]);
+            if (board[i][j] == 'O') {
+                printf("\x1b[31m%c\x1b[0m ", board[i][j]); // 赤でO
+            } else if (board[i][j] == 'X') {
+                printf("\x1b[34m%c\x1b[0m ", board[i][j]); // 青でX
+            } else {
+                printf("%c ", board[i][j]); // 数字はデフォ色
+            }            
         }
         printf("\n");
     }
 }
 
-void convBord(char board[ROWS][COLS], int input) {
+void convBoard(char board[ROWS][COLS], char mark) {
+    char input;
+    printf("Select number: ");
+    scanf(" %c", &input);
+    int i;
+    for (i = 0; i < ROWS; i++) {
+        int j;
+        for (j = 0; j < COLS; j++) {
+            if (input == board[i][j]) {
+                board[i][j] = mark;
+            }
+        }
+    }
+}
 
+void outputResultDrow(void) {
+    printf("Drow!!\n");
+}
+
+void outputMassage(void) {
+    printf("Choose a number to place your mark: \n");
+}
+
+int checkRowsWin(char board[ROWS][COLS], char mark) {
+    
+}
+
+int checkColsWin(char board[ROWS][COLS], char mark) {
+
+}
+
+int checkCrossWin(char board[ROWS][COLS], char mark) {
+
+}
+
+int checkWin(char board[ROWS][COLS], char mark) {
+    if (checkRowsWin(board, mark) == 1) {
+        return 1;
+    }
+
+    if (checkColsWin(board, mark) == 1) {
+        return 1;
+    }
+
+    if (checkCrossWin(board, mark) == 1) {
+        return 1;
+    }
+    return 0;
+}
+
+void winingMessage(char mark) {
+    if (mark == 'O') {
+        printf("Winner: \x1b[31m%c\x1b[0m ", mark);
+    }
+    if (mark == 'X') {
+        printf("Winner: \x1b[34m%c\x1b[0m ", mark);
+    }    
 }
 
 void playGame(void) {
@@ -45,6 +81,32 @@ void playGame(void) {
         {'1', '2', '3'},
         {'4', '5', '6'},
         {'7', '8', '9'}
-    };
-    outputBord(board);
+    };    
+    int turn = 0;
+    int i;
+    for (i = 0; i < MAX_TURN; i++) {
+        outputMassage();
+        outputBoard(board);
+        if (turn % 2 == 0) {
+            convBoard(board, 'O');
+            if (turn >= MIN_TURN_TO_WIN) {
+                if (checkWin(board, 'O') == 1) {
+                    winingMessage('O');
+                    break;
+                }
+            }
+            turn = turn + 1;
+        } else {
+            convBoard(board, 'X');
+            if (turn >= MIN_TURN_TO_WIN) {
+                if (checkWin(board, 'X') == 1) {
+                    winingMessage('X');
+                    break;
+                }
+            }
+            turn = turn + 1;
+        }
+        outputBoard(board);        
+    }
+    outputResultDrow();
 }
--- src/make_game.h
+++ src/make_game.h
@@ -6,17 +6,22 @@
 #include <string.h>
 #include <time.h>
 
-#define MAX_TURN 8
+#define MAX_TURN 9
 #define ROWS 3
 #define COLS 3
+#define MIN_TURN_TO_WIN 5
 
 // gcc src/main.c src/make_game.c -o game
 
-// int decide_turn(void);
-void outputBord(char board[ROWS][COLS]);
-void convBord(char board[ROWS][COLS], int input);
+void outputMassage(void);
+void outputBoard(char board[ROWS][COLS]);
+void convBoard(char board[ROWS][COLS], char mark);
+void outputResultDrow(void);
+int checkWin(char board[ROWS][COLS], char mark);
+int checkRowsWin(char board[ROWS][COLS], char mark);
+int checkColsWin(char board[ROWS][COLS], char mark);
+int checkCrossWin(char board[ROWS][COLS], char mark);
+void winingMessage(char mark);
 void playGame(void);
-// void player_turn(void);
-// void opponent_turn(void);
 
 #endif // MAKE_GAME_H
